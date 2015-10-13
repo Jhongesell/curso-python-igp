34a62140e06e4fd7c65c890d7c18377dd3dda702
Ernesto Anaya <ernestex@gmail.com>
2015-10-12 20:34:52 -0500

    Ejercicio: Imprimir la cartelera del cine

---
+++ ejercicios/cine/cartelera.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# convierte un archivo csv a una lista
+def csv2list(file):
+    return [row.strip().split(',') for row in open(file).readlines()]
+
+# genera cadena para imprimir titulo de la pelicula
+def pelicula2string(texto):
+    lado = '*' * (len(texto)+4)
+    return '%s\n* %s *\n%s\n' % (lado,texto,lado)
+
+# genera cadena para imprimir cine
+def cine2string(texto):
+    return '   %s:\n' % texto.upper()
+
+# genera cadena para imprimir sala
+def sala2string(sala, horarios):
+    return '      Sala %s:  %s' % (sala, '  '.join(horarios))
+
+
+cines = dict(csv2list('cines.csv'))
+peliculas = dict(csv2list('peliculas.csv'))
+funciones = csv2list('funciones.csv')
+
+cartelera = {}
+
+for cid, pid, sala, horario in funciones:
+
+    # Peliculas
+    if not cartelera.has_key(pid):
+        cartelera[pid] = {}
+    cartelera[pid]['titulo'] = peliculas[pid]
+    if not cartelera[pid].has_key('cines'):
+        cartelera[pid]['cines'] = {}
+
+    # Cines
+    if not cartelera[pid]['cines'].has_key(cid):
+        cartelera[pid]['cines'][cid] = {}
+    cartelera[pid]['cines'][cid]['nombre'] = cines[cid]
+    if not cartelera[pid]['cines'][cid].has_key('salas'):
+        cartelera[pid]['cines'][cid]['salas'] = {}
+
+    #salas
+    if not cartelera[pid]['cines'][cid]['salas'].has_key(sala):
+        cartelera[pid]['cines'][cid]['salas'][sala] = []
+    cartelera[pid]['cines'][cid]['salas'][sala].append(horario)
+
+
+
+# Imprimiendo la data
+for pid, pelicula in cartelera.items():
+    print pelicula2string('(%s) %s' % (pid, pelicula['titulo']))
+    for cid, cine in pelicula['cines'].items():
+        print cine2string('(%s) %s' % (cid, cine['nombre']))
+        for sala,horarios in cine['salas'].items():
+            print sala2string(sala, horarios)
+    print '\n\n'
