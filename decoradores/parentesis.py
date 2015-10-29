41d598b1620e33e83cf30c12490739197c6f078a
Ernesto Anaya <ernestex@gmail.com>
2015-10-29 09:07:50 -0500

    Deco: Parentesis sin decorado

---
+++ decoradores/parentesis.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+
+def parentesis(s):
+    return '(%s)' % s
+
+
+def corchetes(s):
+    return '[%s]' % s
+
+
+print parentesis('1')
+print corchetes('2')
